7dcf9831a75fcba78d38066ea5b5215a0d46d329
Pedro Puppin <102486020+pedropuppin@users.noreply.github.com>
2022-10-22 17:10:09 -0300

    Revert "Revert "seeds""

--- app/models/island.rb
+++ app/models/island.rb
@@ -1,3 +1,5 @@
 class Island < ApplicationRecord
   belongs_to :user
+
+  validates :name, :description, :price, :location, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :island
+  has_many :island, dependent: :destroy
+
+  validates :name, :phone, :email, presence: true
+  validates :email, uniqueness: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
